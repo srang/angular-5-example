04042a65edc2a99293087d782c25fa89d62d51c9
srang <srang2010@gmail.com>
2018-06-20 20:43:12 -0400

    test

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ IMAGE=registry.access.redhat.com/rhscl/nginx-112-rhel7
 if [ -d dist/nginx-cfg ]; then
   rm -rf dist/nginx-cfg dist/nginx-default-cfg
 fi
-cp -r nginx/nginx-cfg dist
-cp -r nginx/nginx-default-cfg dist
+cp -r nginx/. dist
 s2i build . $IMAGE ally-nginx-test -c --context-dir dist
 docker run --rm -it -e RUNME=/usr/libexec/s2i/run -p 8080:8080 ally-nginx-test /bin/sh
