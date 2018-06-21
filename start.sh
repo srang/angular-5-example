dfafd79a1982b086b0439c3ef1b37fc1e151354c
srang <srang2010@gmail.com>
2018-06-20 20:51:49 -0400

    .

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ if [ -d dist/nginx-cfg ]; then
   rm -rf dist/nginx-cfg dist/nginx-default-cfg
 fi
 cp -rv nginx/. dist
-s2i build . $IMAGE ally-nginx-test -c --context-dir dist
+s2i build . $IMAGE ally-nginx-test -c --context-dir dist -e NGINX_OVERRIDE_DEFAULT_LOC=true
 docker run --rm -it -e RUNME=/usr/libexec/s2i/run -p 8080:8080 ally-nginx-test /bin/sh
